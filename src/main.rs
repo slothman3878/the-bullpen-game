163da6386e5088134d8df5f4143d9946c627e827
Doyun Kim <65599912+slothman3878@users.noreply.github.com>
2024-10-02 23:58:17 -0700

    assets

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-use std::f32::consts::PI;
-
 use avian3d::prelude::*;
 use bevy::{
     diagnostic::LogDiagnosticsPlugin, input::common_conditions::input_just_released, math::DVec3,
-    prelude::*, window::WindowResolution,
+    prelude::*, window::*,
 };
 use bevy_avian_baseball_flight::prelude::*;
 use bevy_flycam::{FlyCam, NoCameraPlayerPlugin};
 use blenvy::*;
+use sickle_ui::{prelude::*, SickleUiPlugin};
 
 const WINDOW_WIDTH: f32 = 1920.0;
 const WINDOW_HEIGHT: f32 = 1024.0;
@@ -20,6 +19,10 @@ fn main() {
             title: "The Bullpen".to_string(),
             resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
             resizable: false,
+            cursor: Cursor {
+                grab_mode: CursorGrabMode::Locked,
+                ..default()
+            },
             ..Default::default()
         }),
         ..Default::default()
@@ -30,6 +33,7 @@ fn main() {
         app.add_plugins(PhysicsDebugPlugin::default());
     }
 
+    app.add_plugins(SickleUiPlugin);
     app.add_plugins(BlenvyPlugin::default());
     app.add_plugins(PhysicsPlugins::default());
     app.add_plugins(NoCameraPlayerPlugin);
@@ -39,9 +43,139 @@ fn main() {
         drag_on: true,
     });
 
-    app.add_systems(PostStartup, (setup_scene, spawn_camera.after(setup_scene)));
+    app.add_systems(PostStartup, setup);
+    // app.add_systems(PostStartup, (setup_scene, spawn_camera.after(setup_scene)));
 
     app.run();
+}
+
+fn setup(mut commands: Commands) {
+    // The main camera which will render UI
+    let main_camera = commands
+        .spawn((Camera3dBundle {
+            camera: Camera {
+                order: 1,
+                clear_color: Color::BLACK.into(),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0., 30., 0.))
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },))
+        .id();
+
+    // Use the UI builder with plain bundles and direct setting of bundle props
+    let mut root_entity = Entity::PLACEHOLDER;
+    commands.ui_builder(UiRoot).container(
+        (
+            NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    flex_direction: FlexDirection::Column,
+                    justify_content: JustifyContent::SpaceBetween,
+                    ..default()
+                },
+                ..default()
+            },
+            TargetCamera(main_camera),
+        ),
+        |container| {
+            root_entity = container
+                .spawn((NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        flex_direction: FlexDirection::Row,
+                        justify_content: JustifyContent::SpaceBetween,
+                        ..default()
+                    },
+                    ..default()
+                },))
+                .id();
+        },
+    );
+
+    commands.ui_builder(root_entity).spawn(NodeBundle {
+        style: Style {
+            width: Val::Px(200.),
+            border: UiRect::all(Val::Px(2.)),
+            ..default()
+        },
+        background_color: Color::srgb(0.65, 0.65, 0.65).into(),
+        ..default()
+    });
+
+    commands.ui_builder(root_entity).column(|column| {
+        column
+            .style()
+            .height(Val::Percent(100.))
+            .width(Val::Percent(100.))
+            .background_color(Color::srgb(0.15, 0.155, 0.16));
+
+        // column.spawn(NodeBundle {
+        //     style: Style {
+        //         width: Val::Px(200.),
+        //         border: UiRect::all(Val::Px(2.)),
+        //         ..default()
+        //     },
+        //     background_color: Color::srgb(0.65, 0.65, 0.65).into(),
+        //     ..default()
+        // });
+
+        column.menu_bar(|bar| {
+            bar.menu(
+                MenuConfig {
+                    name: "Showcase".into(),
+                    alt_code: KeyCode::KeyS.into(),
+                    ..default()
+                },
+                |menu| {
+                    menu.menu_item(MenuItemConfig {
+                        name: "Layout".into(),
+                        shortcut: vec![KeyCode::KeyL].into(),
+                        alt_code: KeyCode::KeyL.into(),
+                        ..default()
+                    });
+                    menu.menu_item(MenuItemConfig {
+                        name: "Interactions".into(),
+                        shortcut: vec![KeyCode::ControlLeft, KeyCode::KeyI].into(),
+                        alt_code: KeyCode::KeyI.into(),
+                        ..default()
+                    });
+
+                    menu.separator();
+
+                    let icons = ThemeData::default().icons;
+                    menu.menu_item(MenuItemConfig {
+                        name: "Exit".into(),
+                        leading_icon: icons.exit_to_app,
+                        ..default()
+                    });
+                },
+            );
+        });
+    });
+
+    commands.ui_builder(root_entity).row(|row| {
+        row.sized_zone(
+            SizedZoneConfig {
+                size: 100.,
+                ..default()
+            },
+            |zone| {
+                zone.spawn(NodeBundle {
+                    style: Style {
+                        width: Val::Px(200.),
+                        border: UiRect::all(Val::Px(2.)),
+                        ..default()
+                    },
+                    background_color: Color::srgb(0.65, 0.65, 0.65).into(),
+                    ..default()
+                });
+            },
+        );
+    });
 }
 
 fn spawn_camera(mut commands: Commands) {
